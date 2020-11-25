e794918c7f191c56a7036ce3e6e51f9af8797d92
KFischer (mbp_13) <klaus.fischer@eloware.com>
2020-11-25 21:57:14 +0100

    Starting with SwiftUI

--- Home Automation/ContentView.swift
+++ Home Automation/ContentView.swift
@@ -9,10 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            Button("Laden"){
+                do{
+                    let items = try RestService().getItems()
+                    print(items?.names[0])
+                }
+                catch{
+                    print("Error on getting data")
+                }
+            }
+        LazyHGrid(rows: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Rows@*/[GridItem(.fixed(20))]/*@END_MENU_TOKEN@*/) {
+            ItemView(title: "Haustüre", id: "0")
+            ItemView(title: "Haustüre", id: "0")
+            ItemView(title: "Haustüre", id: "0")
+
+        }
+        }
     }
 }
+
+
+struct ItemView: View{
+    
+    let title: String
+    let id: String
+    
+    var body: some View{
+        Button("Name", action: {()->Void in
+               print("Starting \(id)")
+            
+        })
+        .border(Color.black, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+    }
+    
+    
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
