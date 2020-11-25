7abacda59f7b60b276df469679bfdbd3161c4ae2
KFischer (mbp_13) <klaus.fischer@eloware.com>
2020-11-25 20:47:50 +0100

    Initial Commit

---
+++ Home Automation/Home_AutomationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Home_AutomationApp.swift
+//  Home Automation
+//
+//  Created by Klaus Fischer on 25.11.20.
+//
+
+import SwiftUI
+
+@main
+struct Home_AutomationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
