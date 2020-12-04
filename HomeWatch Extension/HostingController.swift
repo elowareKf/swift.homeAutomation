35121d6ed2d1e790e7f74780a2dd78e268b0affd
KFischer(M13) <klaus.fischer@eloware.com>
2020-12-04 07:51:11 +0100

    Added WatchApp

---
+++ HomeWatch Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  HomeWatch Extension
+//
+//  Created by Klaus Fischer on 04.12.20.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ HomeWatch Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  HomeWatch Extension
+//
+//  Created by Klaus Fischer on 04.12.20.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
